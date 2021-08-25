e3b36070ccef2355efa804b8ce6778e4b79a9499
Tr3tyakov <ImTretyakov@mail.ru>
2021-08-25 18:04:14 +0300

    make it better

---
+++ dist/_next/static/chunks/pages/CreateVacancy-070a3a139de8849966e4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[345],{7978:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/CreateVacancy",function(){return u(2891)}])}},function(n){n.O(0,[844,99,491,174,891,774,888,179],(function(){return _=7978,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
