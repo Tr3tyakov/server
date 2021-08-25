7df7f7f20b8c04ca7c3c74d9831a1c468843f235
Tr3tyakov <ImTretyakov@mail.ru>
2021-08-25 19:10:04 +0300

    make it better

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.static('static'));
-app.use(serveStatic(__dirname + '/dist'));
+app.use(serveStatic(__dirname + '/site'));
 
 app.use(
   fileUpload({
