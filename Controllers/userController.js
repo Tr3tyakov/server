6eb2527eac7d9f7cb28f5a0e813cfea6b10f71b0
Tr3tyakov <ImTretyakov@mail.ru>
2021-08-26 12:39:15 +0300

    make it better

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -26,11 +26,9 @@ class UserController {
       res.cookie('refreshToken', userData.refreshToken, {
         maxAge: 30 * 24 * 60 * 1000,
         httpOnly: true,
-        secret: 'street',
-        proxy: true,
+        domain: 'tailwindproject.vercel.app',
         sameSite: 'none',
         secure: true,
-        path: '/',
       });
       res.json(userData);
     } catch (e) {
@@ -55,11 +53,9 @@ class UserController {
       res.cookie('refreshToken', userData.refreshToken, {
         maxAge: 30 * 24 * 60 * 1000,
         httpOnly: true,
-        secret: 'street',
-        proxy: true,
+        domain: 'tailwindproject.vercel.app',
         sameSite: 'none',
         secure: true,
-        path: '/',
       });
       res.json(userData);
     } catch (e) {
