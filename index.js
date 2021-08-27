e913ce567a87595e7541965920b7fdaa426d5af3
Tr3tyakov <ImTretyakov@mail.ru>
2021-08-27 09:45:06 +0300

    make it better

--- index.js
+++ index.js
@@ -7,39 +7,29 @@ const vacancyRouter = require('./Routers/vacancyRouter');
 const resumeRouter = require('./Routers/resumeRouter');
 const ErrorHandler = require('./middleware/errorHandler');
 const cors = require('cors');
+const sslRedirect = require('heroku-ssl-redirect');
 const fileUpload = require('express-fileupload');
 const app = express();
-const appURL = 'https://tailwindproject.vercel.app';
+
 const PORT = process.env.PORT || 5000;
+const appURL = 'https://tailwindproject.vercel.app';
 
 app.use(cors({ origin: appURL, credentials: true }));
-var forceSsl = function (req, res, next) {
-  if (req.headers['x-forwarded-proto'] !== 'https') {
-    return res.redirect(['https://', req.get(appURL), req.url].join(''));
-  }
-  return next();
-};
+app.use(express.json());
+app.use(express.static('static'));
 
-// app.set('trust proxy', 1);
-app.configure(function () {
-  app.use(forceSsl);
+app.use(
+  fileUpload({
+    createParentPath: true,
+  }),
+);
+app.use(sslRedirect());
+app.use(cookieParser());
 
-  app.use(express.json());
-  app.use(express.static('static'));
-
-  app.use(
-    fileUpload({
-      createParentPath: true,
-    }),
-  );
-
-  app.use(cookieParser());
-
-  app.use('/api', userRouter);
-  app.use('/api', resumeRouter);
-  app.use('/api', vacancyRouter);
-  app.use(ErrorHandler);
-});
+app.use('/api', userRouter);
+app.use('/api', resumeRouter);
+app.use('/api', vacancyRouter);
+app.use(ErrorHandler);
 const start = async () => {
   try {
     await mongoose.connect(process.env.DB_URL, {
