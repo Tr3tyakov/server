80b090f0e9636e45e2617b031543b0a0fe5b1838
Tr3tyakov <ImTretyakov@mail.ru>
2021-08-27 10:40:22 +0300

    make it better

--- index.js
+++ index.js
@@ -11,26 +11,13 @@ const fileUpload = require('express-fileupload');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+
 const appURL = 'https://tailwindproject.vercel.app';
-// app.use(cors({ origin: 'appURL', credentials: true }));
-app.enable('trust proxy');
-env = process.env.NODE_ENV || 'development';
-
-var forceSsl = function (req, res, next) {
-  if (req.headers['x-forwarded-proto'] !== 'https') {
-    return res.redirect(['https://', req.get('Host'), req.url].join(''));
-  }
-  return next();
-};
-
-app.configure(function () {
-  if (env === 'production') {
-    app.use(forceSsl);
-  }
-
-  // other configurations etc for express go here...
+app.use(cors({ origin: 'appURL', credentials: true }));
+app.get('*', function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] != 'https')
+    res.redirect('https://tailwindproject.vercel.app' + req.url);
+  else next(); /* Continue to other routes if we're not redirecting */
 });
 app.use(express.json());
 app.use(express.static('static'));
