517c56de22b71c387a6c96c680e459b93ce7e36f
Tr3tyakov <ImTretyakov@mail.ru>
2021-08-27 10:17:38 +0300

    make it better

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ app.get('*', function (req, res, next) {
   if ('https' !== req.headers['x-forwarded-proto'] && 'production' === process.env.NODE_ENV) {
     res.redirect('https://' + req.hostname + req.url);
   } else {
-    // Continue to other routes if we're not redirecting
     next();
   }
 });
