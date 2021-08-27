4e123e42452a43af93be77411b89f3a4fac4a38b
Tr3tyakov <ImTretyakov@mail.ru>
2021-08-27 09:31:50 +0300

    make it better

--- index.js
+++ index.js
@@ -9,42 +9,52 @@ const ErrorHandler = require('./middleware/errorHandler');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
 const app = express();
-app.get('*', function (req, res, next) {
-  if (req.headers['x-forwarded-proto'] != 'https') res.redirect(appURL + req.url);
-  else next(); /* Continue to other routes if we're not redirecting */
-});
-
-const PORT = process.env.PORT || 5000;
 const appURL = 'https://tailwindproject.vercel.app';
-app.use(cors({ origin: appURL, credentials: true }));
-app.set('trust proxy', 1);
+env = process.env.NODE_ENV || 'development';
 
-app.use(express.json());
-app.use(express.static('static'));
-
-app.use(
-  fileUpload({
-    createParentPath: true,
-  }),
-);
-
-app.use(cookieParser());
-
-app.use('/api', userRouter);
-app.use('/api', resumeRouter);
-app.use('/api', vacancyRouter);
-app.use(ErrorHandler);
-const start = async () => {
-  try {
-    await mongoose.connect(process.env.DB_URL, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    });
-    app.listen(PORT, () => console.log(`SERVER HAS BEEN STARTED ${PORT}`));
-  } catch (e) {
-    console.log(e, e.message, 'SERVER');
+var forceSsl = function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get(appURL), req.url].join(''));
   }
+  return next();
 };
 
-start();
+app.configure(function () {
+  if (env === 'production') {
+    app.use(forceSsl);
+  }
+
+  const PORT = process.env.PORT || 5000;
+  app.use(cors({ origin: appURL, credentials: true }));
+  // app.set('trust proxy', 1);
+
+  app.use(express.json());
+  app.use(express.static('static'));
+
+  app.use(
+    fileUpload({
+      createParentPath: true,
+    }),
+  );
+
+  app.use(cookieParser());
+
+  app.use('/api', userRouter);
+  app.use('/api', resumeRouter);
+  app.use('/api', vacancyRouter);
+  app.use(ErrorHandler);
+  const start = async () => {
+    try {
+      await mongoose.connect(process.env.DB_URL, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      });
+      app.listen(PORT, () => console.log(`SERVER HAS BEEN STARTED ${PORT}`));
+    } catch (e) {
+      console.log(e, e.message, 'SERVER');
+    }
+  };
+
+  start();
+});
