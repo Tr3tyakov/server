04323592f4a7265f86466d5caf5adfc9d34c636f
Tr3tyakov <ImTretyakov@mail.ru>
2021-08-27 10:25:51 +0300

    make it better

--- index.js
+++ index.js
@@ -15,9 +15,12 @@ const PORT = process.env.PORT || 5000;
 const appURL = 'https://tailwindproject.vercel.app';
 app.use(cors({ origin: appURL, credentials: true }));
 app.get('*', function (req, res, next) {
-  if (req.headers['x-forwarded-proto'] != 'https')
-    res.redirect('https://tailwindproject.vercel.app' + req.url);
-  else next(); /* Continue to other routes if we're not redirecting */
+  if ('https' !== req.headers['x-forwarded-proto'] && 'production' === process.env.NODE_ENV) {
+    res.redirect('https://' + req.hostname + req.url);
+  } else {
+    // Continue to other routes if we're not redirecting
+    next();
+  }
 });
 app.use(express.json());
 app.use(express.static('static'));
